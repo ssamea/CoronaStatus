bb2ffc87957104c8f58f0410f4035b8e660ef1ea
ssamea <wlstjdals3@gmail.com>
2021-07-27 11:45:33 +0900

    2021.07.2

--- app/src/main/java/com/example/covid/PaymentActivity.java
+++ app/src/main/java/com/example/covid/PaymentActivity.java
@@ -35,7 +35,7 @@ public class PaymentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
-        BootpayAnalytics.init(this, "");
+        BootpayAnalytics.init(this, "60f82a8b23868400210e52cb");
         Intent intent=getIntent();
         payment=findViewById(R.id.payment);
         itemName=findViewById(R.id.itemName);
